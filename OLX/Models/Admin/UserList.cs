74938a0d6422bc7815849e0578935a8c749d565b
aman.gupta <aman.gupta@DT227>
2023-10-09 15:07:57 +0530

    lets check pariksha ki ghadi

---
+++ OLX/Models/Admin/UserList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OLX.Models.Admin
+{
+    public class UserList
+    {
+
+        public int UserId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userEmail { get; set; }
+        public string Password { get; set; }
+        public string MobileNo { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public DateTime createdon { get; set; }
+        public DateTime updatedon { get; set; }
+
+
+    }
+}
